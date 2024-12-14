d5878b0d5f68be07c8c8c4d567083bfd7c4ebdac
Khajornsak2545 <khajornsak.s@ku.th>
2024-12-14 22:45:36 +0700

    Fix configuration for Vercel

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 1. ติดตั้ง npm dependencies
+# ติดตั้ง npm dependencies
 npm install
 
-# 2. สร้างไฟล์ assets (CSS/JS) สำหรับ production
+# สร้าง assets (CSS/JS) สำหรับ production
 npm run build
 
-# 3. ติดตั้ง PHP dependencies
+# ติดตั้ง PHP dependencies
 composer install --no-dev --optimize-autoloader
